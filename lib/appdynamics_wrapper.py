8fcf3c20ae06dc2f3ae0d31c56a53aa4ea491539
Pranjal Kumar <pranjal.kumar@appdynamics.com>
2015-12-29 10:53:12 +0530

    Update appdynamics_wrapper.py

--- lib/appdynamics_wrapper.py
+++ lib/appdynamics_wrapper.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 
 import sys
+import json
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
+
+base_path = sys.argv[1] 
+json_object = sys.argv[2]
+print json.load(json_object)
+
+
+
+
