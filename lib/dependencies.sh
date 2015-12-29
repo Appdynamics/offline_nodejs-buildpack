a5914f9f069422951971b759b7dc4a8c940a124d
Pranjal Kumar <pranjal.kumar@appdynamics.com>
2015-12-29 16:38:54 +0530

    Update dependencies.sh

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -41,10 +41,10 @@ install_appd_modules() {
         VCAP_SERVICES="$VCAP_SERVICES"
         echo $VCAP_SERVICES
         LEN=$(echo ${#VCAP_SERVICES})
-        if [ LEN -lt 2 ]; then
+        if [ $LEN -lt 2 ]; then
                 echo "doesn't have at least 2 characters"
         else
-                python $BP_DIR/lib/appdynamics_wrapper.py  $BP_DIR $VCAP_SERVICES
+                python $BP_DIR/lib/appdynamics_wrapper.py $BP_DIR $VCAP_SERVICES
         fi
         #if [ ! -z $VCAP_SERVICES ]; then
         #  python $BP_DIR/lib/appdynamics_wrapper.py  $BP_DIR '{"appdynamics":[{"name":"node_js","label":"appdynamics","tags":["appdynamics","apm","mobile real-user monitoring","browser real-user monitoring","database monitoring","server monitoring","application analytics"],"plan":"Gold","credentials":{"account-name":"customer1","port":"8090","account-access-key":"e47c0e60-6e7d-41ad-8c64-0ae0d2f6708b","host-name":"52.33.129.11","ssl-enabled":"false"}}]}'
