a7835e213dd7cab0df89dd05f74f9c8574cccb97
pkumar-appd <pranjal.kumar@appdynamics.com>
2015-12-29 11:05:41 +0530

    added extension call

--- lib/appdynamics_wrapper.py
+++ lib/appdynamics_wrapper.py
@@ -2,6 +2,13 @@
 
 import sys
 import json
+import os
+import os.path
+import tempfile
+import shutil
+from nose.tools import eq_
+from nose.tools import with_setup
+from build_pack_utils import utils
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
@@ -9,6 +16,10 @@ print 'Argument List:', str(sys.argv)
 base_path = sys.argv[1] 
 json_object = sys.argv[2]
 print json.load(json_object)
+
+
+appdynamics = utils.load_extension('extensions')
+
 
 
 
