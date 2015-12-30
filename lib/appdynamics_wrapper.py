024963ec09bac31068d0e6599bf01ca751ec6b99
Pranjal Kumar <pranjal.kumar@appdynamics.com>
2015-12-30 09:25:06 +0530

    Update appdynamics_wrapper.py

--- lib/appdynamics_wrapper.py
+++ lib/appdynamics_wrapper.py
@@ -6,7 +6,7 @@ import os
 import os.path
 import tempfile
 import shutil
-from build_pack_utils.cloudfoundry import CloudFoundryUtil
+from build_pack_utils import utils
 
 print 'Number of arguments:', len(sys.argv), 'arguments.'
 print 'Argument List:', str(sys.argv)
@@ -15,23 +15,8 @@ base_path = sys.argv[1]
 json_object = sys.argv[2]
 print json.loads(json_object)
 
-CloudFoundryUtil.initialize()
+
+
 appdynamics = utils.load_extension(os.path.join(base_path,'extensions'))
+ad = appdynamics.AppDynamicsInstaller(utils.FormattedDict(json.loads(json_object)))
 ad = appdynamics.AppDynamicsInstaller(json.loads(json_object))
-eq_(True, ad.should_install())
-eq_('x64', ad._php_arch)
-eq_('@{HOME}/php/lib/php/extensions/no-debug-non-zts-20121212',
-    ad._php_extn_dir)
-eq_(False, ad._php_zts)
-eq_('20121212', ad._php_api)
-eq_('@{HOME}/appdynamics/agent/x64/appdynamics-20121212.so', ad.appdynamics_so)
-eq_('app-name-2', ad.app_name)
-eq_('LICENSE2', ad.license_key)
-eq_('@{HOME}/logs/appdynamics-daemon.log', ad.log_path)
-eq_('@{HOME}/appdynamics/daemon/appdynamics-daemon.x64', ad.daemon_path)
-eq_('@{HOME}/appdynamics/daemon.sock', ad.socket_path)
-eq_('@{HOME}/appdynamics/daemon.pid', ad.pid_path)
-
-
-
-
