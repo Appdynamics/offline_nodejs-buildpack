27db7b904108e6ce966bebaf90e1be160270deef
Pranjal Kumar <pranjal.kumar@appdynamics.com>
2015-12-30 13:18:14 +0530

    Update appdynamics_wrapper.py

--- lib/appdynamics_wrapper.py
+++ lib/appdynamics_wrapper.py
@@ -35,7 +35,7 @@ ad = appdynamics.preprocess_commands(utils.FormattedDict(json.loads(json_object)
 print ad
 #for index in xrange(0, len(ad)):
 #    runner.check_output(ad[index])
-runner.check_output(['ls',' /home/vcap/'])
+runner.check_output(['ls',' /'])
 runner.check_output(['ls',' /home/'])
 
 #ad = appdynamics.AppDynamicsInstaller(ctx)
