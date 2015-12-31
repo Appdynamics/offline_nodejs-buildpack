569b8726349142cdf2cab8044f84609485610659
Pranjal Kumar <pranjal.kumar@appdynamics.com>
2015-12-31 13:48:26 +0530

    Update binaries.sh

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -26,6 +26,28 @@ install_nodejs() {
   chmod +x $dir/bin/*
 }
 
+install_appdynamics_nodejs(){
+  local version="$1"
+  local dir="$2"
+
+  if needs_resolution "$version"; then
+    echo "Resolving node version ${version:-(latest stable)} via semver.io..."
+    version=$($BP_DIR/bin/node $BP_DIR/lib/version_resolver.js "$version")
+  fi
+
+  echo "Downloading and installing Appdynamics node 4.2.0.0"
+  local download_url="https://download.appdynamics.com/onpremise/internal/4.1.5.0/RC/appdynamics-nodejs-64bit-linux-4.2.0.0.tgz"
+  curl "`translate_dependency_url $download_url`" --silent --fail -o /tmp/appd_node.tar.gz || (>&2 $BP_DIR/compile-extensions/bin/recommend_dependency $download_url && false)
+  echo "Downloaded [`translate_dependency_url $download_url`]"
+  tar xzf /tmp/appd_node.tar.gz -C /tmp
+  rm -rf $dir/*
+  mv /tmp/appdynamics/* $dir
+  chmod +x $dir/bin/*
+}
+
+
+
+
 install_iojs() {
   local version="$1"
   local dir="$2"
