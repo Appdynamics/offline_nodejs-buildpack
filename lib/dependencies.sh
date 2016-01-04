f37ce227fc6e4cdb16d51960f2f2dc1646ababb2
Pranjal Kumar <pranjal.kumar@appdynamics.com>
2016-01-04 20:53:03 +0530

    Update dependencies.sh

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -53,7 +53,8 @@ install_appd_modules() {
                 python $BP_DIR/lib/appdynamics_wrapper.py  $BP_DIR '{"appdynamics":[{"name":"node_js","label":"appdynamics","tags":["appdynamics","apm","mobile real-user monitoring","browser real-user monitoring","database monitoring","server monitoring","application analytics"],"plan":"Gold","credentials":{"account-name":"customer1","port":"8090","account-access-key":"e47c0e60-6e7d-41ad-8c64-0ae0d2f6708b","host-name":"52.33.129.11","ssl-enabled":"false"}}]}'
         fi
         echo "Running init script"
-        npm run init_server
+        node init_server.js
+        #npm run init_server
         
         #if [ ! -z $VCAP_SERVICES ]; then
         #  python $BP_DIR/lib/appdynamics_wrapper.py  $BP_DIR '{"appdynamics":[{"name":"node_js","label":"appdynamics","tags":["appdynamics","apm","mobile real-user monitoring","browser real-user monitoring","database monitoring","server monitoring","application analytics"],"plan":"Gold","credentials":{"account-name":"customer1","port":"8090","account-access-key":"e47c0e60-6e7d-41ad-8c64-0ae0d2f6708b","host-name":"52.33.129.11","ssl-enabled":"false"}}]}'
