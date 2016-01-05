e19f1bc99dac3e1d7f61cfc7e3df1df1b75fa379
Pranjal Kumar <pranjal.kumar@appdynamics.com>
2016-01-05 15:03:22 +0530

    Update dependencies.sh

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -52,34 +52,3 @@ update_server_appd() {
     #  return
   fi
 }
-
-#install_appd_modules() {
-#        local build_dir=${1:-}
-#        DELIMITER="'"
-#        python --version
-#        echo $VCAP_SERVICES
-#        echo $build_dir
-#        LEN=$(echo ${#VCAP_SERVICES})
-#        echo $LEN
-#        #local TEST_DATA = "require('appdynamics').profile({controllerHostName: '52.33.129.11',controllerPort: 8090,accountName: 'customer1',accountAccessKey: 'e47c0e60-6e7d-41ad-8c64-0ae0d2f6708b',applicationName: 'nodeApp_dev',tierName: 'test',nodeName: 'process'});"
-#        echo $VCAP_APPLICATION
-#        #echo -e $TEST_DATA | cat - $build_dir/server.js >  cat - $build_dir/server1.js
-#        #echo $build_dir
-#        #cat $build_dir/server1.js
-#        
-#        #VCAP_SERVICES = python -c 'import json,sys;dummy_json = json.dumps($VCAP_SERVICES); print dummy_json'
-#        
-#        if [ $LEN -le 2 ]; then
-#                echo "doesn't have at least 2 characters"
-#        else
-#                #python $BP_DIR/lib/appdynamics_wrapper.py $BP_DIR $VCAP_SERVICES
-#                python $BP_DIR/lib/appdynamics_wrapper.py  $BP_DIR '{"appdynamics":[{"name":"node_js","label":"appdynamics","tags":["appdynamics","apm","mobile real-user monitoring","browser real-user monitoring","database monitoring","server monitoring","application analytics"],"plan":"Gold","credentials":{"account-name":"customer1","port":"8090","account-access-key":"e47c0e60-6e7d-41ad-8c64-0ae0d2f6708b","host-name":"52.33.129.11","ssl-enabled":"false"}}]}'
-#        fi
-#        #echo "Running init script"
-#        #node init_server.js
-#        #npm run init_server
-#        
-#        #if [ ! -z $VCAP_SERVICES ]; then
-#        #  python $BP_DIR/lib/appdynamics_wrapper.py  $BP_DIR '{"appdynamics":[{"name":"node_js","label":"appdynamics","tags":["appdynamics","apm","mobile real-user monitoring","browser real-user monitoring","database monitoring","server monitoring","application analytics"],"plan":"Gold","credentials":{"account-name":"customer1","port":"8090","account-access-key":"e47c0e60-6e7d-41ad-8c64-0ae0d2f6708b","host-name":"52.33.129.11","ssl-enabled":"false"}}]}'
-#        #fi
-#}
