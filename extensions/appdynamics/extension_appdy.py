c14c29f8c214d0b007cbe297e654e0afd066a1fe
Pranjal Kumar <pranjal.kumar@appdynamics.com>
2016-01-28 08:47:28 -0800

    Update extension_appdy.py

--- extensions/appdynamics/extension_appdy.py
+++ extensions/appdynamics/extension_appdy.py
@@ -3,6 +3,7 @@ import sys
 import json
 import os.path
 import logging
+import subprocess
 
 class Appdynamics_ext:
     def get_vcap_args(self):
@@ -17,22 +18,28 @@ class Appdynamics_ext:
     
         with open(vcap_application_filename) as data_file:
             self.VCAP_APPLICATION = json.load(data_file)
+            
+    def set_environ_variables(self):
+        setEnv = "cf set-env %s http_proxy %s"%(self.applicationName, self.httpProxy)
+        subprocess.call(setEnv)
     
     def generate_appdy_statement(self):
         self.get_vcap_args()
     
-        extension_name = "appdynamics"
-        controllerHostName = self.VCAP_SERVICES["appdynamics"][0]["credentials"]["host-name"]
-        controllerPort = self.VCAP_SERVICES["appdynamics"][0]["credentials"]["port"]
-        accountName = self.VCAP_SERVICES["appdynamics"][0]["credentials"]["account-name"]
-        accountAccessKey = self.VCAP_SERVICES["appdynamics"][0]["credentials"]["account-access-key"]
-        applicationName = self.VCAP_APPLICATION["name"]
-        tierName = "test"
-        nodeName = "process"
-        
+        self.extension_name = "appdynamics"
+        self.controllerHostName = self.VCAP_SERVICES["appdynamics"][0]["credentials"]["host-name"]
+        self.controllerPort = self.VCAP_SERVICES["appdynamics"][0]["credentials"]["port"]
+        self.accountName = self.VCAP_SERVICES["appdynamics"][0]["credentials"]["account-name"]
+        self.accountAccessKey = self.VCAP_SERVICES["appdynamics"][0]["credentials"]["account-access-key"]
+        self.applicationName = os.getenv('APPDYNAMICS_APP_NAME') or self.VCAP_APPLICATION["name"]
+        self.tierName = self.VCAP_APPLICATION["name"]
+        self.nodeName = self.tierName
+        self.httpProxy= os.environ.get('HTTP_PROXY') or os.environ.get('HTTPS_PROXY')
+        if self.httpProxy:
+            self.set_environ_variables()
         require_statement = """require('%s').profile({ controllerHostName: 
             '%s',controllerPort: %s, accountName: '%s', accountAccessKey: '%s', 
-            applicationName: '%s',tierName: '%s',nodeName: '%s'});""" % (extension_name, controllerHostName, controllerPort, accountName, accountAccessKey, applicationName, tierName, nodeName)
+            applicationName: '%s',tierName: '%s',nodeName: '%s'});""" % (self.extension_name, self.controllerHostName, self.controllerPort, self.accountName, self.accountAccessKey, self.applicationName, self.tierName, self.nodeName)
     
         print require_statement
         
