d1315c1b300c9d6373fad89ced0a1bfde7c6b742
Pranjal Kumar <pranjal.kumar@appdynamics.com>
2016-02-08 09:54:55 -0800

    Update dependencies.sh

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -34,23 +34,23 @@ rebuild_node_modules() {
   fi
 }
 
+BP_DIR=$(cd $(dirname ${0:-}); cd ..; pwd)
+
 get_file_initial() {
   local FILE_NAME=$(python -c 'import json,sys; f = open("package.json","r"); obj = json.load(f);print obj["scripts"]["start"].split()[1]; f.close()')
   echo $FILE_NAME
 }
-
-
-BP_DIR=$(cd $(dirname ${0:-}); cd ..; pwd)
 
 update_server_appd() {
   local build_dir=${1:-}
   LEN=$(echo ${#VCAP_SERVICES})
   if [ $LEN -ge 4 ]; then
     echo "Reading Environment Variables for Appdynamics"
-    echo $VCAP_SERVICES > $build_dir/_vcap_services.txt
-    echo $VCAP_APPLICATION > $build_dir/_vcap_application.txt
+    echo $VCAP_SERVICES > /tmp/_vcap_services.txt
+    echo $VCAP_APPLICATION > /tmp/_vcap_application.txt
     local INIT_FILE=$(get_file_initial)
-    local TEST_DATA=$(python $BP_DIR/extensions/appdynamics/extension_appdy.py $build_dir)
+    python $BP_DIR/extensions/appdynamics/extension_appdy.py $build_dir
+    local TEST_DATA=$(cat /tmp/_vcap_application.txt)
     echo $TEST_DATA | cat - $build_dir/$INIT_FILE >  $build_dir/tmp.js && mv $build_dir/tmp.js $build_dir/$INIT_FILE
   fi
 }
